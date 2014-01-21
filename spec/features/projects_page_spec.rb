4ed00d5d83ac43130daaeef03dda3189cbf63a39
3yatsu <ms018a.k@gmail.com>
2014-01-21 20:35:48 +0900

    プロジェクト新規作成画面追加

--- spec/features/projects_page_spec.rb
+++ spec/features/projects_page_spec.rb
@@ -6,5 +6,9 @@ describe "プロジェクト一覧ページ" do
     before { visit projects_path }
     it { should have_content('New Project') }
 
+    describe "新規作成画面へ遷移" do
+      before { click_link('New Project') }
+      it { should have_content('New Project') }
+    end
   end
 end
